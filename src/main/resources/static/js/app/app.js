ea1c29b312d7005abd0f15c67c8adacce137f0f9
shevchuk-d <authentic.d@gmail.com>
2017-03-31 01:56:55 +0300

    fe

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -10,5 +10,15 @@ app.controller('customersCtrl', function($scope, $http) {
         $scope.reservedLockers = response.data;
     });
 
+    $http.get("http://localhost:8080/hunk/lockers/reserved/neighbors").then(function(response) {
+        $scope.reservedLockersNeighbors = response.data;
+    });
+
+    $scope.arrayContains = function (array, obj) {
+        // console.log(JSON.stringify(array).indexOf(JSON.stringify(obj)));
+        // console.log(JSON.stringify(array));
+        // console.log(JSON.stringify(obj));
+        return angular.toJson(array).indexOf(angular.toJson(obj)) >= 0;
+    }
 });
 
